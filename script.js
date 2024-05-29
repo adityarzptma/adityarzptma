11d0d8286640e62426e741aa30177a0b62bedd68
adityarzptma <madityas5e@gmail.com>
2024-05-29 08:10:40 +0700

    web pertama

---
+++ script.js
@@ -0,0 +1,9 @@
+		<script>
+			function redirInstagram() {
+				window.location.href = "https://instagram.com/adityarzptma?igshid=MzNlNGNkZWQ4Mg=="
+			}
+			
+			function redirWhatsapp() {
+				window.location.href = "https://wa.me/+6285701720681"
+			}
+		</script>
